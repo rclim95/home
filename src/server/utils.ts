b45f091c6aea07842c36e4c8f4bb8ae0529726b6
Richard Lim <hello@rclim95.com>
2021-07-23 17:30:58 -0500

    Choose a random (recently-played) game for Steam
    Rather than always returning the first element in the
    GetRecentlyPlayedGames() API call to Steam, choose a game in the list at
    random to make it more interesting. :smile:

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,10 +1,25 @@
 import config from "../config/config.json";
 import http from "http";
 import { Game } from "../types";
+import { isArray } from "lodash";
 
 const BASE_URL_STORE = "https://store.steampowered.com/app/";
 const BASE_URL_ICON = "http://media.steampowered.com/steamcommunity/public/images/apps/";
 const BASE_URL_RECENTLY_PLAYED_GAMES = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?format=json";
+
+/**
+ * Chooses an element within `array` randomly.
+ * @param array The array to choose an element from.
+ * @returns One of the element inside `array`. If `array` is empty, then `null` is returned.
+ */
+export function choose<TElement>(array: TElement[]): TElement | null {
+    if (array.length === 0) {
+        return null;
+    }
+
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
 
 /**
  * Gets the recently played game for a user (defined in `config.json`) on Steam.
@@ -12,12 +27,20 @@ const BASE_URL_RECENTLY_PLAYED_GAMES = "http://api.steampowered.com/IPlayerServi
  */
 export function getRecentlyPlayedGame(): Promise<Game | null> {
     return new Promise((resolve, reject) => {
-        let url = `${BASE_URL_RECENTLY_PLAYED_GAMES}&key=${config.steam.key}&steamid=${config.steam.userid}&count=1`;
+        let url = `${BASE_URL_RECENTLY_PLAYED_GAMES}&key=${config.steam.key}&steamid=${config.steam.userid}`;
         http.get(url, (res) => {
             res.on("data", d => {
                 let jsonResponse = JSON.parse(d);
-                let recentGame = jsonResponse["response"]["games"][0];
-                if (recentGame !== undefined) {
+                let games: any[] = jsonResponse["response"]["games"];
+
+                // If Steam's API doesn't return an array for some reason, set to an empty array
+                // by default.
+                if (!isArray(games)) {
+                    games = [];
+                }
+
+                let recentGame = choose(games);
+                if (recentGame !== null) {
                     return resolve({
                         name: recentGame["name"],
                         storeUrl: `${BASE_URL_STORE}${recentGame["appid"]}`,
